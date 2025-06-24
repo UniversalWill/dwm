8c5573f6414a3777dd6a0a03411835eaa659f20b
Gennadiy Ivashchenko <gena9902@gmail.com>
2025-06-24 10:32:01 +0500

    feat: change default terminal to alacritty

--- config.h
+++ config.h
@@ -66,7 +66,8 @@ static char dmenumon[2] =
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
-static const char *termcmd[] = {"kitty"};
+// static const char *termcmd[] = {"kitty"};
+static const char *termcmd[] = {"alacritty"};
 static const char *up_vol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@",
                                "+10%", NULL};
 static const char *down_vol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@",
